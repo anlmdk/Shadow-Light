4bd6aae0fb1b3ddaee203d72a1210e415a6ee506
Anıl <47008814+anlmdk@users.noreply.github.com>
2023-12-05 01:35:36 +0300

    fixed speed and music

--- Assets/Audio.cs
+++ Assets/Audio.cs
@@ -1,17 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Audio : MonoBehaviour
 {
+    public Slider volumeSlider;
+    public AudioSource soundSource;
 
     void Start()
     {
         DontDestroyOnLoad(this);
+
+        // Match the initial volume of the sound source with the slider value
+        soundSource.volume = volumeSlider.value;
+
+        // Add a listener to the slider's value changed event
+        volumeSlider.onValueChanged.AddListener(delegate { SetVolume(); });
     }
 
-    void Update()
+    void SetVolume()
     {
-        
+        // Update the volume of the sound source with the slider value
+        soundSource.volume = volumeSlider.value;
     }
 }
